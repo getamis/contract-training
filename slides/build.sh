1caa2936209bb15861206ae9d95c6f57d52df42d
Yute Lin <yutelin@gmail.com>
2017-02-20 12:03:39 +0800

    Update script location

--- slides/build.sh
+++ slides/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -name '*.md' -not -path "./node_modules/*" | xargs -I '{}' markdown-to-slides '{}'  -o '{}'.html -s css/style.css
+find . -name '*.md' -not -path "./node_modules/*" | xargs -I '{}' markdown-to-slides '{}'  -o '{}'.html -s css/style.css --script js/remark-latest.min.js
