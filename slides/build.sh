ba8588e1ebbc0f69701cb57c115af3a9071e233a
Yute Lin <yutelin@gmail.com>
2017-02-20 12:11:35 +0800

    Include source in slide building

--- slides/build.sh
+++ slides/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -name '*.md' -not -path "./node_modules/*" | xargs -I '{}' markdown-to-slides '{}'  -o '{}'.html -s css/style.css --script js/remark-latest.min.js
+find . -name '*.md' -not -path "./node_modules/*" | xargs -I '{}' markdown-to-slides '{}'  -o '{}'.html -s css/style.css -i
